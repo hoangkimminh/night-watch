f489f5eaf59cb934772fe3319cd330961c6c0bed
Hoàng Đức <47973648+DucNguyen98@users.noreply.github.com>
2019-07-02 23:51:47 +0700

    [f1] Add package app-server (#1)

---
+++ packages/app-server/README.md
@@ -0,0 +1,20 @@
+# NIGHT WATCH CRAWLER
+
+## INSTALLATION
+
+### Requirements
+
+- Node.js >= 8
+
+### Instructions
+
+```bash
+$ yarn install
+$ yarn start # yarn dev for development
+```
+
+## DOCUMENTATION
+
+| ROUTE  | METHOD | REQ BODY                                | RES BODY               |
+| ------ | ------ | --------------------------------------- | ---------------------- |
+| `/`    | GET    |                                         | `{ iam: '/' }`         |
---
+++ packages/app-server/src/server.js
@@ -0,0 +1,19 @@
+const fastify = require('fastify')
+
+const loggerLevel = process.env.NODE_ENV !== 'production' ? 'debug' : 'info'
+const server = fastify({ ignoreTrailingSlash: true, logger: { level: loggerLevel } })
+
+server.get('/', async () => {
+  return { iam: '/' }
+})
+
+const start = async () => {
+  try {
+    await server.listen(process.env.PORT || 3000, '::') // listen to all IPv6 and IPv4 addresses
+  } catch (err) {
+    server.log.error(err)
+    process.exit(1)
+  }
+}
+
+start()
