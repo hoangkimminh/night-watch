1b811384173ef65180d1831a7efc045ab0e03810
hoangkimminh <vatliyeutoi@gmail.com>
2019-07-10 23:18:12 +0700

    [f4] Add Bulma CSS to package web-app

--- packages/web-app/pages/index.js
+++ packages/web-app/pages/index.js
@@ -1,2 +1,14 @@
-const Index = () => <div>Welcome to night-watch</div>
+import 'bulma/css/bulma.css'
+import { Component } from 'react'
+
+class Index extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <div className='row'>
+        </div>
+      </div>
+    )
+  }
+}
 export default Index
